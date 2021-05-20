8b5e4a1c5f081be242b2fa951c3d32f0f19bfbe4
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2021-05-20 22:55:09 +0300

    Private_Msg2
    Part2

--- ChatRoomClient/src/chatmsg/Login.java
+++ ChatRoomClient/src/chatmsg/Login.java
@@ -31,6 +31,7 @@ public class Login extends javax.swing.JFrame {
     public int RivalSelection = -1;
     //benim seçimim seçim -1 deyse seçilmemiş
     public int myselection = -1;
+    public String selectedUserForPrvChat="";
     HashMap<String, String> myPrivateChats = new HashMap<String, String>();
     Random rand;
     //
@@ -71,20 +72,41 @@ public class Login extends javax.swing.JFrame {
     }
 
     public void updateMyUserList(DefaultListModel dlm) {
-        // Aktif kullanıcıların listelenmesi
-        list_all_users.setModel(dlm);
+        // Aktif kullanıcıların listelenmesi   
+        String selected=list_all_users.getSelectedValue();
+        list_all_users.setModel(dlm);  
+        boolean isChanged=true;
+        if(selected!=null){
+        for (int i = 0; i < dlm.size(); i++) {
+            String uname=(String)dlm.get(i);
+             
+            if(selected.equals(uname)){
+                list_all_users.setSelectedIndex(i);
+               System.out.println(selected+"-"+uname+":"+i);
+                isChanged=false;
+                break;
+            }
+        }}
+        if(isChanged)list_all_users.setSelectedIndex(0);
         createNewPrivateChat(dlm);
     }
     public void createNewPrivateChat(DefaultListModel dlm){
         String username=(String)dlm.get(dlm.getSize()-1);
         if(!myPrivateChats.containsKey(username)){
-             myPrivateChats.put(username, "Baslangic");
+             myPrivateChats.put(username, "");
         }
        
     }
     public void privateMsgReceived(PrivateMsg pmsg){
-        myPrivateChats.put(pmsg.getSender(), myPrivateChats.get(pmsg.getSender())+"\n"+pmsg.getSender()+": "+pmsg.getContent());
-        txt_private_chat.setText(myPrivateChats.get(pmsg.getSender()));
+        String oldMsgs="";
+        if(myPrivateChats.get(pmsg.getSender())!=null)oldMsgs=myPrivateChats.get(pmsg.getSender());
+        
+        
+        myPrivateChats.put(pmsg.getSender(), oldMsgs+pmsg.getSender()+": "+pmsg.getContent()+"\n");
+        if(pmsg.getSender().equals(list_all_users.getSelectedValue())){
+            txt_private_chat.setText(myPrivateChats.get(pmsg.getSender()));
+        }
+        
         lbltest.setText(pmsg.getContent());
                
     }
@@ -169,9 +191,14 @@ public class Login extends javax.swing.JFrame {
             public int getSize() { return strings.length; }
             public String getElementAt(int i) { return strings[i]; }
         });
+        list_all_users.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                list_all_usersValueChanged(evt);
+            }
+        });
         jScrollPane1.setViewportView(list_all_users);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 110, 170, 240));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 110, 210, 240));
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel1.setText("GLOBAL CHAT");
@@ -188,7 +215,7 @@ public class Login extends javax.swing.JFrame {
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jLabel2.setText("Online Users");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 80, -1, -1));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 80, -1, -1));
 
         btn_send_private.setText("Send");
         btn_send_private.addActionListener(new java.awt.event.ActionListener() {
@@ -209,10 +236,10 @@ public class Login extends javax.swing.JFrame {
         getContentPane().add(txt_private_chat, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 110, 260, 170));
 
         jButton1.setText("jButton1");
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1060, 160, -1, -1));
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1180, 150, -1, -1));
 
         lbltest.setText("label1");
-        getContentPane().add(lbltest, new org.netbeans.lib.awtextra.AbsoluteConstraints(940, 30, -1, -1));
+        getContentPane().add(lbltest, new org.netbeans.lib.awtextra.AbsoluteConstraints(990, 30, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -270,12 +297,15 @@ public class Login extends javax.swing.JFrame {
 
     private void btn_send_privateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_send_privateActionPerformed
         // TODO add your handling code here:
-        //txt_private_chat.setText(txt_private_chat.getText()+"\n"+txt_myusername.getText()+": "+txt_msg_private.getText());
+        
         String senderUsername = txt_myusername.getText();
         String targetUsername=list_all_users.getSelectedValue();
         String content=txt_msg_private.getText();
         PrivateMsg pmsg=new PrivateMsg(senderUsername, targetUsername, content);
-        myPrivateChats.put(targetUsername, myPrivateChats.get(targetUsername)+"\n"+senderUsername+": "+content);
+        String oldMsgs="";
+        if(myPrivateChats.get(pmsg.getTarget())!=null)oldMsgs=myPrivateChats.get(targetUsername);
+        
+        myPrivateChats.put(targetUsername, oldMsgs+senderUsername+": "+content+"\n");
         txt_private_chat.setText(myPrivateChats.get(targetUsername));
         txt_msg_private.setText("");
         
@@ -286,6 +316,25 @@ public class Login extends javax.swing.JFrame {
         
             
     }//GEN-LAST:event_btn_send_privateActionPerformed
+
+    private void list_all_usersValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_list_all_usersValueChanged
+        // TODO add your handling code here:
+        
+        selectedUserForPrvChat=list_all_users.getSelectedValue();
+        if(selectedUserForPrvChat!=null && selectedUserForPrvChat.equals(txt_myusername.getText())){
+        txt_private_chat.setText("");
+        txt_private_chat.setEnabled(false);
+        txt_msg_private.setEnabled(false);
+        btn_send_private.setEnabled(false);
+        }else{
+        txt_private_chat.setText(myPrivateChats.get(selectedUserForPrvChat));
+        txt_private_chat.setEnabled(true);
+        txt_msg_private.setEnabled(true);
+        btn_send_private.setEnabled(true);
+        }
+        
+        
+    }//GEN-LAST:event_list_all_usersValueChanged
 
     /**
      * @param args the command line arguments
